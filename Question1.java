4ec7e8682d2807344cfe2e3564cfbe5b2a6a59f4
Vinay-Tare <44465147+Vinay-Tare@users.noreply.github.com>
2020-10-17 22:16:54 +0530

    Question1 Solution Added

---
+++ Question1.java
@@ -0,0 +1,18 @@
+/**
+* 1. Write a program that produces the following output:  
+
+*Hello World! 
+
+*It's been nice knowing you.
+
+*Goodbye world!   
+*/
+
+public class Question1 {
+    public static void main(String[] args){
+        System.out.println("Hello World! \n\n" + 
+        "It's been nice knowing you. \n\n" +
+        "Goodbye world! \n\n");
+    }
+    
+}
